c2eac0b745b3247c23ee1939bdbff1ca4bb7eb03
ethan <yi.fang@myshape.ai>
2023-07-31 14:12:32 +0800

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1799693.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 142928276, 142928277, 142928278, 142928279, 142933434, 142939565, 142939815, 142955352, 142955353, 142955354, 143317033, 143959894 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/142928300/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/142928301/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/142928299/1/ammo.js', 'preload' : true},
+];
